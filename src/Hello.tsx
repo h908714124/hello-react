dfb91e3724716cea03e1877f0b4e327f6edb84a8
jbock <jbock-java@gmx.de>
2023-06-16 13:24:51 +0200

    add some form data

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -8,13 +8,28 @@ export default function Hello() {
 
   return (
     <div>
-      <p><input onInput={onTextInputModified} value={name}></input></p>
+      <p><input onInput={onNameModified} value={name}></input></p>
+      <p><input onInput={onEmailModified} defaultValue={model.email}></input></p>
+      <p><input onInput={onNumberModified} defaultValue={model.number}></input></p>
       <p>{name}</p>
+      <p><button type='submit' onClick={onOkButtonClicked}>OK</button></p>
     </div>
   );
 
-  function onTextInputModified(e: FormEvent<HTMLInputElement>) {
+  function onNameModified(e: FormEvent<HTMLInputElement>) {
     setName(e.currentTarget.value);
     model.name = e.currentTarget.value;
+  }
+
+  function onEmailModified(e: FormEvent<HTMLInputElement>) {
+    model.email = e.currentTarget.value;
+  }
+
+  function onNumberModified(e: FormEvent<HTMLInputElement>) {
+    model.number = e.currentTarget.value;
+  }
+
+  function onOkButtonClicked() {
+    alert(JSON.stringify(model));
   }
 }
