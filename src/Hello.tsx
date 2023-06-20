753beeaf3af51aca37b39e4e2555b066eaa0ccc7
jbock <jbock-java@gmx.de>
2023-06-20 11:25:00 +0200

    use form

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,16 +1,22 @@
 import { FormEvent, useState } from 'react';
 import { useContext } from './index';
+import { useForm } from 'react-hook-form'
 
 export default function Hello() {
 
   const model = useContext().model.hello;
   const [name, setName] = useState(model.name);
+  const { register }  = useForm({defaultValues : {
+    name: model.name,
+    email : model.email,
+    number : model.number
+  }});
 
   return (
     <div>
-      <p><input onInput={onNameModified} value={name}></input></p>
-      <p><input onInput={onEmailModified} defaultValue={model.email}></input></p>
-      <p><input onInput={onNumberModified} defaultValue={model.number}></input></p>
+      <p><input {...register("name")} onInput={onNameModified}></input></p>
+      <p><input {...register("email")} onInput={onEmailModified}></input></p>
+      <p><input {...register("number")} onInput={onNumberModified}></input></p>
       <p>{name}</p>
       <p><button type='submit' onClick={onOkButtonClicked}>OK</button></p>
     </div>
